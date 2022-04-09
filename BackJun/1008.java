c386fef3d7362adff3d469ad729ff72b1628d8ee
kosas3771 <kosas3771@gmail.com>
2022-04-09 18:35:44 +0900

    E1008

---
+++ BackJun/1008.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner aa = new Scanner(System.in);
+        
+        double a = aa.nextDouble();
+        double b = aa.nextDouble();
+
+        System.out.println(a/b);
+
+    }
+
+}
