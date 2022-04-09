197a0d7f745e5b4735c69cc82ed045974bbcd71a
kosas3771 <kosas3771@gmail.com>
2022-04-09 18:48:21 +0900

    E10869

---
+++ BaekJoon/10869.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner fog = new Scanner(System.in);
+
+        int a = fog.nextInt();
+        int b = fog.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+    
+}
