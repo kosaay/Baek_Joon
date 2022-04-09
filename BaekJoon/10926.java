8fc6ac6b5f15870329aee4164ed15c4bebfdf9ee
kosas3771 <kosas3771@gmail.com>
2022-04-09 19:22:39 +0900

    3rd

---
+++ BaekJoon/10926.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner oop = new Scanner(System.in);
+
+        String id = oop.next();
+
+        System.out.println(id + "??!")
+
+    }
+    
+}
