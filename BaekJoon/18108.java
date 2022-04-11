4bd75b5e4cbbbb169e7660746b535152ec29aa36
kosas3771 <kosas3771@gmail.com>
2022-04-12 08:52:20 +0900

    E18108

---
+++ BaekJoon/18108.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class 18108 {
+    public static void main(String[] arg) {
+        //불기=서기+543
+        Scanner ar = new Scanner(System.in);
+        int AD = ar.nextInt();
+
+        System.out.println(AD-543);
+
+    }
+}
